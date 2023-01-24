8896fa8b4ed76975775258b93e1a89a38a055c4d
Tatiana Rudikova <tatjana_tv@list.ru>
2023-01-24 17:32:07 +0300

    добавление тестов

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -34,4 +34,38 @@ public class OrderCardTest {
         $("[type=button]").click();
         $(".input_type_text .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
+    @Test
+    public void shouldFormTestEmptyPhone() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldFormTestInvalidName() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input").setValue("+79111111111");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldFormTestHyphenName() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Иванов Иван-Тест");
+        $("[data-test-id=phone] input").setValue("+79111111111");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
 }
