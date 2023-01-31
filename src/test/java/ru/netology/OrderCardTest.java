6a01fc77e61015b71a63b8dd8056ab1c71ab2412
Tatiana Rudikova <tatjana_tv@list.ru>
2023-02-01 01:09:23 +0300

    updating tests

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -16,56 +16,90 @@ public class OrderCardTest {
 
     @Test
     public void shouldSubmitFormTest() {
-        $(".form_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
+        $("[class='button__text']").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    public void shouldFormTestEmptyName() {
+    public void shouldEmptyNameField() {
 
-        $(".form_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("");
         $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $(".input_type_text .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    public void shouldFormTestEmptyPhone() {
+    public void shouldHyphenInNameField() {
 
-        $(".form_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=name] input").setValue("Николай Миклухо-Маклай");
+        $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $(".input_type_tel .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[class='button__text']").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    public void shouldFormTestInvalidName() {
+    public void shouldLatinLettersInNameField() {
 
-        $(".form_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("Ivanov Ivan");
         $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    public void shouldFormTestHyphenName() {
+    public void shouldEmptyPhoneField() {
 
-        $(".form_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Иванов Иван-Тест");
-        $("[data-test-id=phone] input").setValue("+79111111111");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[class='button__text']").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldPhoneWithoutPlusSymbol() {
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("89111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldMoreElevenCharactersInPhone() {
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+791111111119");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldLessElevenCharactersInPhone() {
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+7911111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldCheckboxNotMarked() {
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79111111111");
+        $("[class='button__text']").click();
+        $(".input_invalid .checkbox__text").shouldHave(text("Я соглашаюсь"));
     }
 
 }
